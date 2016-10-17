f942a689999a8eb5227c755901b9f600c1470e14
Anton <krylovsentry@yandex.ru>
2016-10-17 22:25:10 +0300

    Delete excess code

--- instagram_inject.js
+++ instagram_inject.js
@@ -2,33 +2,13 @@
  * Created by ankr0416 on 13.05.2016.
  */
 (function () {
-
-
-
-    var articles = document.getElementsByTagName('a');
-
-
-
-
-
-    for(var i = 0 ; i < 1000 ; i++){
-
-        if(document.getElementById('pImage_' + i) != null) {
-
-            var img = document.getElementById('pImage_' + i);
-            img.parentNode.parentNode.parentNode.parentNode.appendChild(createLink(img,i));
-        }
-
-    }
-
-
     function createLink(img,count) {
 
         var url = img.getAttribute('src');
         var id = img.getAttribute('id');
         var link = document.createElement('a');
         //create link
-        link.className = 'downloadLink';
+        link.className = 'download_link';
         link.setAttribute('href', url);
         link.setAttribute('title', "Download");
         link.setAttribute('download', 'FILE_DOWNLOAD_' + count);
@@ -47,39 +27,19 @@
         } else return true;
 
     }
-
     function createNewImg() {
-
-        var images = document.getElementsByTagName('img');
-
+        //Take all images from document
+        var images = document.images;
 
         for(var j = 0 ; j < images.length; j ++) {
-
             var id = images[j].getAttribute('id');
-            if (id != null) {
-                var number = id.substr(7);
-
-
-                    if (document.getElementById('pImage_' + number) != null) {
-
-                        var img = document.getElementById('pImage_' + number);
-                        if (checkOldImg(img)) {
-
-                            img.parentNode.parentNode.parentNode.parentNode.appendChild(createLink(img, i));
-
-                        }
-                    }
-
-
-
+            var image = images[j]
+            if (id) {
+                if (checkOldImg(image)) {
+                    image.parentNode.parentNode.parentNode.parentNode.appendChild(createLink(image, j));
+                }
             }
         }
     }
-
     setInterval(createNewImg,10000);
-
-
-
-
-
 })();
